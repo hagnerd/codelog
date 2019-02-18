0d2c434435954c9e03a435ddc3acc518c3ba8c8a
Matt Hagner <web@matthagner.com>
2019-02-18 11:56:16 -0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-923271824e66316512b8.js"
+    "url": "webpack-runtime-8b609badda53fdacb644.js"
   },
   {
     "url": "styles.b4a0cbda3eb007e6ec25.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "340c3f5f026b46555da7ad90009a489a"
+    "revision": "72d0b75447bbf9cbd4ab4fe01b3dbfb6"
   },
   {
     "url": "component---src-pages-404-js-759f95949d19cd33cf3e.js"
